11148b5ec5a42e05e7c85f0fd669e1ea917e108e
Suyash Sharma <sharmasuyash821+cc@gmail.com>
2025-04-08 15:44:11 +0530

    feat(services): added services page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import ContactSection from "@/app/_components/contact-section";
 import HeroSection from "@/app/_components/hero-section";
-import ServicesSection from "@/app/_components/services-section";
+import ServicesSection from "@/components/services-section";
 import SkillsSection from "@/app/_components/skills-section";
 
 export default function Home() {
